68b3d357442ab7f0c620d971dfc65f86ceadc5e8
Muhammad Yusuf <yusuftkr077@gmail.com>
2025-11-23 21:00:32 +0700

    belajar view data laravel yusuf

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,16 @@
 <x-layout>
-     <x-slot:title>{{ $title }}</x-slot:title>
+    <x-slot:title>{{ $title }}</x-slot:title>
     <h3 class="text-xl">ini adalah halaman Blog Page</h3>
+
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-2xl tracking-tight font-bold text-gray-900">Judul Artikel</h2>
+        <div class="text-base text-gray-500">
+            <a href="#">Muhammad Yusuf</a> | <time datetime="2024-06-01">23 November 2025</time>
+        </div>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus modi error repellendus labore! Laborum
+            libero fugit dolorem culpa aliquid, eius ea. Aspernatur in maiores ipsam minima, architecto corrupti enim
+            error?</p>
+        <a href="" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+    </article>
 </x-layout>
